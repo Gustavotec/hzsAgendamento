6d21fc023a32f5459418d84d32d732589ae9ea2b
Gustavotec <gustavotec@gmail.com>
2022-03-28 00:01:24 -0300

    Formulário

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import { ReactComponent as GithubIcon } from "assets/img/Vector.svg";
+import './styles.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="agendamento-nav-content">
+          <h1>Fila Cirurgica</h1>
+          <a
+            href="https://hospitalzonasul.saude.pr.gov.br"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="agendamento-contact-container">
+              <GithubIcon />
+              <p className="agendamento-contact-link">Hospital Zona Sul</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
